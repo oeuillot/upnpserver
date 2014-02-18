e3409c1eb2095f3dee1d71b72119149eb466c5a0
vf <vf_info@mail.ru>
2014-02-18 15:51:24 +0400

    Add exports

---
+++ index.js
@@ -0,0 +1,6 @@
+/*jslint node: true */
+"use strict";
+
+module.exports.UPNPServer = require("./lib/upnpServer");
+module.exports.PathRepository = require("./lib/pathRepository");
+module.exports.MusicRepository = require("./lib/musicRepository");
